5e7b24c66a644e086090863a3ff7c003d8987ce2
Chuanqi Chen <chuanqi.chen@gmail.com>
2024-01-14 23:06:54 -0600

    modified:   CS237B_HW1/Problem_1/value_iteration.py

--- CS237B_HW1/Problem_1/value_iteration.py
+++ CS237B_HW1/Problem_1/value_iteration.py
@@ -40,6 +40,51 @@ def value_iteration(problem, reward, terminal_mask, gam):
 
     return V
 
+def calculate_optimal_policy(problem, reward, gam, V_opt):
+    Ts = problem["Ts"]
+    sdim, adim = Ts[0].shape[-1], len(Ts)  # state and action dimension
+    V = tf.zeros([sdim])
+
+    assert reward.ndim == 2
+    Q = reward + gam*tf.einsum('ans,s->na', tf.convert_to_tensor(Ts), V_opt)
+    Policy_opt = tf.argmax(Q, axis=1)
+    return Policy_opt
+
+def visualize_policy_function(V, Policy):
+    """
+    Visualizes the value function given in V & computes the optimal action,
+    visualized as an arrow.
+
+    You need to call plt.show() yourself.
+
+    Args:
+        V: (np.array) the value function reshaped into a 2D array.
+    """
+    V = np.array(V)
+    assert V.ndim == 2
+    m, n = V.shape
+    pos2idx = np.arange(m * n).reshape((m, n))
+    X, Y = np.meshgrid(np.arange(m), np.arange(n))
+    pts = np.stack([X.reshape(-1), Y.reshape(-1)], -1)
+    u, v = [], []
+    for pt in pts:
+        pt_min, pt_max = [0, 0], [m - 1, n - 1]
+        pt_right = np.clip(np.array(pt) + np.array([1, 0]), pt_min, pt_max)
+        pt_up = np.clip(np.array(pt) + np.array([0, 1]), pt_min, pt_max)
+        pt_left = np.clip(np.array(pt) + np.array([-1, 0]), pt_min, pt_max)
+        pt_down = np.clip(np.array(pt) + np.array([0, -1]), pt_min, pt_max)
+        next_pts = [pt_right, pt_up, pt_left, pt_down]
+        idx = Policy[pt[0], pt[1]]
+        #Vs = [V[next_pt[0], next_pt[1]] for next_pt in next_pts]
+        #idx = np.argmax(Vs)
+        u.append(next_pts[idx][0] - pt[0])
+        v.append(next_pts[idx][1] - pt[1])
+    u, v = np.reshape(u, (m, n)), np.reshape(v, (m, n))
+
+    plt.imshow(V.T, origin="lower")
+    plt.quiver(X, Y, u, v, pivot="middle")
+
+
 
 # value iteration ##############################################################
 def main():
@@ -64,6 +109,12 @@ def main():
     plt.figure(213)
     visualize_value_function(np.array(V_opt).reshape((n, n)))
     plt.title("value iteration")
+    plt.show()
+
+    Policy_opt = calculate_optimal_policy(problem, reward, gam, V_opt)
+    plt.figure(215)
+    visualize_policy_function(np.array(V_opt).reshape((n, n)), np.array(Policy_opt).reshape((n, n)))
+    plt.title("Optimal Policy")
     plt.show()
 
 
