22d6f32220aa9e0c4c55f744302461b504f754f9
Chuanqi Chen <chuanqi.chen@gmail.com>
2024-03-04 11:51:19 -0600

    modified:   train_il.py

--- CS237B_HW3/train_il.py
+++ CS237B_HW3/train_il.py
@@ -17,8 +17,12 @@ class NN(tf.keras.Model):
         #         - tf.keras.initializers.GlorotUniform (this is what we tried)
         #         - tf.keras.initializers.GlorotNormal
         #         - tf.keras.initializers.he_uniform or tf.keras.initializers.he_normal
-
-
+        l1_l2 = tf.keras.regularizers.l1_l2(l1=0.01, l2=0.001)
+        self.dense1 = tf.keras.layers.Dense(250, activation=tf.nn.leaky_relu, kernel_regularizer=l1_l2, bias_regularizer=l1_l2,kernel_initializer=tf.keras.initializers.GlorotUniform())
+        self.dropout1 = tf.keras.layers.Dropout(0.25)
+        self.dense2 = tf.keras.layers.Dense(50, activation=tf.nn.leaky_relu, kernel_regularizer=l1_l2, bias_regularizer=l1_l2, kernel_initializer=tf.keras.initializers.GlorotUniform())
+        self.dropout2 = tf.keras.layers.Dropout(0.25)
+        self.dense3 = tf.keras.layers.Dense(out_size, kernel_regularizer=l1_l2, bias_regularizer=l1_l2, kernel_initializer=tf.keras.initializers.GlorotUniform())
 
         ########## Your code ends here ##########
 
@@ -27,13 +31,16 @@ class NN(tf.keras.Model):
         ######### Your code starts here #########
         # We want to perform a forward-pass of the network. Using the weights and biases, this function should give the network output for x where:
         # x is a (?,|O|) tensor that keeps a batch of observations
-
-
+        out = self.dense1(x)
+        out = self.dropout1(out)
+        out = self.dense2(out)
+        out = self.dropout2(out)
+        return self.dense3(out)
 
         ########## Your code ends here ##########
 
 
-def loss(y_est, y):
+def loss(y_est, y, goal):
     y = tf.cast(y, dtype=tf.float32)
     ######### Your code starts here #########
     # We want to compute the loss between y_est and y where
@@ -41,9 +48,18 @@ def loss(y_est, y):
     # - y is the actions the expert took for the corresponding batch of observations
     # At the end your code should return the scalar loss value.
     # HINT: Remember, you can penalize steering (0th dimension) and throttle (1st dimension) unequally
+    if(goal=="left"):
+        #weight_steering = 20.0
+        weight_steering = 5
+    elif (goal=="right"):
+        #weight_steering = 35
+        weight_steering = 3
+    else:
+        weight_steering = 10
+    weight_throttle = 1.0
+    _loss = tf.reduce_sum(tf.reduce_mean(tf.math.square(y_est - y), axis=0)*(tf.convert_to_tensor([weight_steering, weight_throttle])))
 
-
-
+    return _loss; 
     ########## Your code ends here ##########
     
 
@@ -73,8 +89,13 @@ def nn(data, args):
         # 3. Based on the loss calculate the gradient for all weights
         # 4. Run an optimization step on the weights.
         # Helpful Functions: tf.GradientTape(), tf.GradientTape.gradient(), tf.keras.Optimizer.apply_gradients
+        with tf.GradientTape() as tape:
+            y_est = nn_model(x)
+            current_loss = loss(y_est, y, args.goal) +  tf.add_n(nn_model.losses)
+        grads = tape.gradient(current_loss, nn_model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, nn_model.trainable_variables))
         
-        
+        #optimizer.minimize(current_loss, nn_model.trainable_weights)
 
         ########## Your code ends here ##########
 
