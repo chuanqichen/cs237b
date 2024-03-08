769c727e923eb449a07eb111495c2ba47f24d705
Chuanqi Chen <chuanqi.chen@gmail.com>
2024-03-08 15:17:17 -0600

    modified:   run_train_ildist_lane.sh

--- CS237B_HW3/run_train_ildist_lane.sh
+++ CS237B_HW3/run_train_ildist_lane.sh
@@ -1,9 +1,12 @@
 echo "*** right ***"
 echo "python train_ildist.py --scenario lanechange --goal right --epochs 1000 --lr 5e-3"
-python train_ildist.py --scenario lanechange --goal right --epochs 1000 --lr 5e-3
-#python train_ildist.py --scenario lanechange --goal right --epochs 28000 --lr 1e-5 --restore
+python train_ildist.py --scenario lanechange --goal right --epochs 10 --lr 1e-3
+python train_ildist.py --scenario lanechange --goal right --epochs 100 --lr 1e-5 --restore
+python train_ildist.py --scenario lanechange --goal right --epochs 1000 --lr 1e-5 --restore
 
 echo "*** left ***"
 echo "python train_ildist.py --scenario lanechange --goal left --epochs 1000 --lr 5e-3"
-python train_ildist.py --scenario lanechange --goal left --epochs 1000 --lr 5e-3 
+python train_ildist.py --scenario lanechange --goal left --epochs 10 --lr 1e-3 
+python train_ildist.py --scenario lanechange --goal left --epochs 100 --lr 1e-5 --restore
+python train_ildist.py --scenario lanechange --goal left --epochs 1000 --lr 1e-5 --restore
 #python train_ildist.py --scenario lanechange --goal left --epochs 28000 --lr 1e-5 --restore
