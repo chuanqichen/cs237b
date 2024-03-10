e0cf00ea30dd04c0f58cb238746c971946f160d8
Chuanqi Chen <chuachen@adcorp.kla-tencor.com@MI1-COMGA1001.hpc.kla-tencor.com>
2024-03-10 18:59:19 -0400

    new file:   CS237B_HW3/policies/intersection_all_CoIL.data-00000-of-00001 	new file:   CS237B_HW3/policies/intersection_all_CoIL.index 	new file:   CS237B_HW3/policies/lanechange_left_ILDIST.data-00000-of-00001 	new file:   CS237B_HW3/policies/lanechange_left_ILDIST.index 	new file:   CS237B_HW3/policies/lanechange_right_ILDIST.data-00000-of-00001 	new file:   CS237B_HW3/policies/lanechange_right_ILDIST.index 	modified:   CS237B_HW3/run_train_coil.sh

--- CS237B_HW3/run_train_coil.sh
+++ CS237B_HW3/run_train_coil.sh
@@ -1,6 +1,5 @@
 
 #python train_coil.py --scenario intersection --epochs 1000 --lr 5e-3
-#python train_coil.py --scenario intersection --epochs 1000 --lr 1e-5 --restore
 
 python test_coil.py --scenario intersection --goal left
 
